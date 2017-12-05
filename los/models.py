ba635c414bb3d645ad492634f933918a8f5aa504
Mariusz Jakubowski <mariusz.jakubowski.79@gmail.com>
2017-12-05 14:23:56 +0100

    added on_delete for django 2.0 compatibility

--- los/models.py
+++ los/models.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 
 class Wish(models.Model):
     name = models.CharField(max_length=100)
-    person = models.ForeignKey(User)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __unicode__(self):
         return self.name
 
 
 class Choice(models.Model):
-    buyer = models.ForeignKey(User, related_name="buyer")
-    receiver = models.ForeignKey(User, related_name="receiver")
+    buyer = models.ForeignKey(User, related_name="buyer", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name="receiver", on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.buyer.username + " -> " + self.receiver.username
